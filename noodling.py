ebb0c144d7a0a4fc5347b7ba551090a07a6d9874
Todd Small <todd_small@icloud.com>
2024-11-11 20:57:44 -0800

    Drummer and Hubble
    stack-info: PR: https://github.com/ToddSmall/credible-data-science/pull/8, branch: ToddSmall/stack/4

---
+++ noodling.py
@@ -0,0 +1,12 @@
+"""Noodling to test stack-pr"""
+
+def drummer() -> str:
+    return "Drummer is cool!"
+
+
+def hubble() -> str:
+    return "Hubble is cool! 🐶"
+
+
+def drummer_and_hubble() -> str:
+    return "Drummer and Hubble are cool!"
